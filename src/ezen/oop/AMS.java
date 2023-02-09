1e686e259d7490a15601e08bf189531f31bf9c38
찬울 <bokdol8@naver.com>
2023-02-09 14:49:08 +0900

    second commit

--- src/ezen/oop/AMS.java
+++ src/ezen/oop/AMS.java
@@ -1,9 +1,15 @@
 package ezen.oop;
 
+/**
+ * 찬울
+ * @author 82105
+ *
+ */
 public class AMS {
 
 	public static void main(String[] args) {
 		System.out.println("ddddd");
+		//git 수정해보기
 	}
 
 }
---
+++ src/ezen/oop/test.java
@@ -0,0 +1,13 @@
+package ezen.oop;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("ddddd");
+		System.out.println("ddddd");
+		System.out.println("ddddd");
+		System.out.println("ddddd");
+		System.out.println("ddddd");
+	}
+
+}
